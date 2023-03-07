7d6ed032970c55c40f68c99727e19e42ddf48ece
Arthur <sdasd@gmail.com>
2023-03-07 16:51:22 +0200

    Func task is done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 "use strict";
 
-const numOfFilms = +prompt ("Сколько фильмов вы уже посмотрели?", "");
+let numOfFilms;
+
+function start() {
+	numOfFilms = +prompt ("Сколько фильмов вы уже посмотрели?", "");
+	
+	while (numOfFilms == "" || numOfFilms == null || isNaN(numOfFilms)) {
+		numOfFilms = +prompt ("Сколько фильмов вы уже посмотрели?", "");
+	}
+}
+
+start();
+
 
 const personalMovieDB = {
 	count: numOfFilms,
+	
 	movies: {
 
 	},
@@ -12,15 +24,67 @@ const personalMovieDB = {
 	privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
+function showMyDb (hidden) {
+	if(!hidden) {
+		console.log(personalMovieDB);
+	}
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+
+function rememberMyFilms() {
+	for (let i = 0; i<2;i++) {
+		const a = prompt("Один из последних просмотренных фильмов?", ""),
+		b = prompt("На сколько оцените его?", "");
+		if(a != null && b != null && a != "" && b != "" && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+		} else {
+			console.log("error");
+			i--;
+		}
+	
+		
+	}
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel () {
+	if(numOfFilms >=0 &&  numOfFilms< 10) {
+		console.log("Просмотрено мало фильмов");
+	} else if(numOfFilms >=10 && numOfFilms <30) {
+		console.log("Вы классический зритель");
+	} else if (numOfFilms >=30) {
+		console.log("Вы киноман");
+	} else {
+		console.log("Ошибка");
+	}
+}
+
+detectPersonalLevel();
+
+function writeUrGenres() {
+	for(let i = 1;i<=3;i++){
+		const a = prompt(`Ваш любимый жанр под номером ${i}`);
+		if(a != null &&  a != "" && a.length < 50) {
+			personalMovieDB.genres[i-1]= a;
+	}
+}
+}
+
+writeUrGenres();
+
+showMyDb(personalMovieDB.privat);
+
+
+
+
+
+
+
+
+
+
 
 
 
